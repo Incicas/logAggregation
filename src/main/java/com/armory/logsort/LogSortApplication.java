15fd7e53acdacebe371e47c9a70cd852f59079db
Beniamin Incicas <74560817+Incicas@users.noreply.github.com>
2022-04-30 12:32:29 +0300

    First Commit

--- src/main/java/com/armory/logsort/LogSortApplication.java
+++ src/main/java/com/armory/logsort/LogSortApplication.java
@@ -3,24 +3,26 @@ package com.armory.logsort;
 import com.armory.logsort.generator.LogGenerator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
+import java.io.*;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 public class LogSortApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // generate log files
-        new LogGenerator(1, 10).generate("./src/main/resources/generated/logs/");
+        new LogGenerator(100, 100).generate("./src/main/resources/generated/logs/");
+
 
         // read log files
-        final List<File> logFiles = Arrays.asList(new File("./src/main/resources/generated/logs/").listFiles());
+        final List<File> logFile1 = Arrays.asList(Objects.requireNonNull(new File("./src/main/resources/generated/logs/").listFiles()));
 
         final Instant start = Instant.now();
 
-        new LogPrinter().printLogs(logFiles);
+        new LogPrinter().printLogs(logFile1);
 
         final Instant finish = Instant.now();
         System.out.println("\nLog sorting and printing took: " + (finish.toEpochMilli() - start.toEpochMilli()) + "ms");
